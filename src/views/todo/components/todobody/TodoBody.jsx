c17e169f7fdf8698558691291f5b4ce58af589b6
Sven Wehrend <swehrend@ant-neuro.com>
2026-05-13 15:52:21 +0200

    added basic styling (part 2)

--- src/views/todo/components/todobody/TodoBody.jsx
+++ src/views/todo/components/todobody/TodoBody.jsx
@@ -1,10 +1,10 @@
 import TodoItem from "../todoitem/TodoItem";
-
 import { deleteTodoItemFromList } from "../../../todolist/TodoList";
+import "./TodoBody.css";
 
 function TodoBody({ todos, handleChangeCheckbox, deleteTodoItemFromList }) {
   return (
-    <div>
+    <div class="todo-body">
       {todos.map((todo) => (
         <TodoItem
           key={todo.id} // Add this line
